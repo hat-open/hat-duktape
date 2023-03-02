11fe6ee31b1c7b49a9bcb74b5e7c6b72fc005f75
bozo.kopic <bozo@kopic.xyz>
2023-03-02 18:08:07 +0100

    playground dist update

--- playground/dist.sh
+++ playground/dist.sh
@@ -4,7 +4,10 @@ set -e
 
 . $(dirname -- "$0")/env.sh
 
-TARGET_PLATFORMS="linux_x86_64 linux_aarch64 windows_amd64"
+TARGET_PLATFORMS="linux_gnu_x86_64
+                  linux_musl_x86_64
+                  linux_gnu_aarch64
+                  windows_amd64"
 
 cd $ROOT_PATH
 rm -rf $DIST_PATH
@@ -12,6 +15,7 @@ mkdir -p $DIST_PATH
 
 for TARGET_PLATFORM in $TARGET_PLATFORMS; do
     export TARGET_PLATFORM
+    $PYTHON -m doit clean_all
     $PYTHON -m doit
     cp $ROOT_PATH/build/py/dist/*.whl $DIST_PATH
 done
